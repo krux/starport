2239e4e1f82a19242dc31246f0b9c2407e32587a
Yuxi Zhang <yuxi.zhang@yuxizhang-ltm.internal.salesforce.com>
2018-12-20 14:06:43 -0800

    add migration script

---
+++ migration-scripts/v6.sql
@@ -0,0 +1,9 @@
+create table "pipelineDependencies" ("pipeline_id" INTEGER NOT NULL,"dependent_pipeline_id" INTEGER NOT NULL);
+
+alter table "pipelineDependencies" add constraint "pipeline_dependencies_pipelines_dependency_fk" foreign key("dependent_pipeline_id") references "pipelines"("id") on update RESTRICT on delete CASCADE;
+alter table "pipelineDependencies" add constraint "pipeline_dependencies_pipelines_fk" foreign key("pipeline_id") references "pipelines"("id") on update RESTRICT on delete CASCADE;
+
+create table "pipeline_histories" ("pipeline_id" INTEGER NOT NULL,"next_run_time" TIMESTAMP,"status" VARCHAR(40) NOT NULL);
+
+alter table "pipeline_histories" add constraint "pipeline_histories_pk" primary key("pipeline_id","next_run_time");
+alter table "pipeline_histories" add constraint "pipeline_histories_pipelines_fk" foreign key("pipeline_id") references "pipelines"("id") on update RESTRICT on delete CASCADE;
