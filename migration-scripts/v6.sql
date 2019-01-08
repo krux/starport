1ffec8b0a7ef29bbddf89e6dbf07e0e4a74ca000
Yuxi Zhang <yuxi.zhang@yuxizhang-ltm.internal.salesforce.com>
2019-01-07 17:16:25 -0800

    update v6 script

--- migration-scripts/v6.sql
+++ migration-scripts/v6.sql
@@ -3,7 +3,7 @@ create table "pipeline_dependencies" ("pipeline_id" INTEGER NOT NULL,"dependent_
 alter table "pipeline_dependencies" add constraint "pipeline_dependencies_pipelines_dependency_fk" foreign key("dependent_pipeline_id") references "pipelines"("id") on update RESTRICT on delete CASCADE;
 alter table "pipeline_dependencies" add constraint "pipeline_dependencies_pipelines_fk" foreign key("pipeline_id") references "pipelines"("id") on update RESTRICT on delete CASCADE;
 
-create table "pipeline_histories" ("pipeline_id" INTEGER NOT NULL,"next_run_time" TIMESTAMP,"status" VARCHAR(40) NOT NULL);
+create table "pipeline_progresses" ("pipeline_id" INTEGER NOT NULL,"progress" VARCHAR(40) NOT NULL);
 
-alter table "pipeline_histories" add constraint "pipeline_histories_pk" primary key("pipeline_id","next_run_time");
-alter table "pipeline_histories" add constraint "pipeline_histories_pipelines_fk" foreign key("pipeline_id") references "pipelines"("id") on update RESTRICT on delete CASCADE;
+alter table "pipeline_progresses" add constraint "pipeline_progresses_pk" primary key("pipeline_id");
+alter table "pipeline_progresses" add constraint "pipeline_progresses_pipelines_fk" foreign key("pipeline_id") references "pipelines"("id") on update RESTRICT on delete CASCADE;
