31a6df69c8eecef3c640b63dd66bf8561fd4f438
Yuxi Zhang <yuxi.zhang@yuxizhang-ltm.internal.salesforce.com>
2019-01-08 11:12:47 -0800

    add tools to manage pipeline dependency and progress status

---
+++ starport-core/src/main/scala/com/krux/starport/db/tool/MarkPipelineAsFinished.scala
@@ -0,0 +1,36 @@
+package com.krux.starport.db.tool
+
+import slick.jdbc.PostgresProfile.api._
+
+import com.krux.starport.config.StarportSettings
+import com.krux.starport.db.WaitForIt
+import com.krux.starport.util.{PipelineProgressHelper, ProgressStatus}
+import com.krux.starport.Logging
+import com.krux.starport.db.table.PipelineProgresses
+
+
+object MarkPipelineAsFinished extends WaitForIt with Logging {
+
+  implicit val starportSettings = StarportSettings()
+
+  lazy val db = starportSettings.jdbc.db
+
+  def showStatus(pipelineId: Int): Unit = {
+    val query = PipelineProgresses()
+      .filter(_.pipelineId === pipelineId)
+      .map(_.progress)
+    val progress = db.run(query.result).waitForResult.headOption
+    logger.info(s"Pipeline $pipelineId progress -> ${progress.getOrElse("Not Found")}")
+  }
+
+  def markAsSuccess(pipelineId: Int): Unit = {
+    new PipelineProgressHelper().insertOrUpdatePipelineProgress(pipelineId, ProgressStatus.SUCCESS)
+    logger.info(s"Marked pipeline $pipelineId as SUCCESS")
+  }
+
+  def main(args: Array[String]): Unit = args.toList match {
+    case "show" :: pipelineIds => pipelineIds.foreach(pid => showStatus(pid.toInt))
+    case "mark" :: pipelineIds => pipelineIds.foreach(pid => markAsSuccess(pid.toInt))
+    case _ => println("Illegal arguments")
+  }
+}
