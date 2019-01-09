5ffab329cd53825def83aecfe25183c8dba8b096
Yuxi Zhang <yuxi.zhang@yuxizhang-ltm.internal.salesforce.com>
2019-01-09 14:48:59 -0800

    update script

--- migration-scripts/v6.sql
+++ migration-scripts/v6.sql
@@ -1,6 +1,6 @@
-create table "pipeline_dependencies" ("pipeline_id" INTEGER NOT NULL,"dependent_pipeline_id" INTEGER NOT NULL);
+create table "pipeline_dependencies" ("pipeline_id" INTEGER NOT NULL,"upstream_pipeline_id" INTEGER NOT NULL);
 
-alter table "pipeline_dependencies" add constraint "pipeline_dependencies_pipelines_dependency_fk" foreign key("dependent_pipeline_id") references "pipelines"("id") on update RESTRICT on delete CASCADE;
+alter table "pipeline_dependencies" add constraint "pipeline_dependencies_pipelines_dependency_fk" foreign key("upstream_pipeline_id") references "pipelines"("id") on update RESTRICT on delete CASCADE;
 alter table "pipeline_dependencies" add constraint "pipeline_dependencies_pipelines_fk" foreign key("pipeline_id") references "pipelines"("id") on update RESTRICT on delete CASCADE;
 
 create table "pipeline_progresses" ("pipeline_id" INTEGER NOT NULL,"progress" VARCHAR(40) NOT NULL);
