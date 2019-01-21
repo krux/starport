b0353987d0e073a40a823ba5364fcc5b1137a10c
Trent Albright <trent.albright@gmail.com>
2019-01-20 22:22:56 -0500

    fix: add logger

--- starport-lambda/src/main/scala/com/krux/starport/lambda/Handler.scala
+++ starport-lambda/src/main/scala/com/krux/starport/lambda/Handler.scala
@@ -2,10 +2,14 @@ package com.krux.starport.lambda
 
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 import com.krux.starport.Starport
+import org.slf4j.LoggerFactory
 
 class Handler extends RequestHandler[Request, Response] {
 
+  def logger = LoggerFactory.getLogger(getClass)
+
   def handleRequest(input: Request, context: Context): Response = {
+    logger.info("lambda invoked...")
     val args = new Array[String](0)
     Starport.main(args)
     Response("Starport lambda handler: ", input)
