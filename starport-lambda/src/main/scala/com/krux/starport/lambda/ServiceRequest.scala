06779320e0dce780fe84ad9b48267a9a66c458da
Trent Albright <trent.albright@gmail.com>
2019-02-03 16:33:46 -0500

    remove service handler lambda for now

--- starport-lambda/src/main/scala/com/krux/starport/lambda/ServiceRequest.scala
+++
@@ -1,7 +0,0 @@
-package com.krux.starport.lambda
-
-import scala.beans.BeanProperty
-
-class ServiceRequest(@BeanProperty var args: Array[String]) {
-  def this() = this(Array[String]())
-}
--- starport-lambda/src/main/scala/com/krux/starport/lambda/ServiceResponse.scala
+++
@@ -1,5 +0,0 @@
-package com.krux.starport.lambda
-
-import scala.beans.BeanProperty
-
-case class ServiceResponse(@BeanProperty message: String, @BeanProperty request: ServiceRequest)
