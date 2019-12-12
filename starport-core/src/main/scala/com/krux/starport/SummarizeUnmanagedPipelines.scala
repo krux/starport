6c2637b471f5b8d14a25e1b20e7304715dd2f48d
Sheng-Loong Su <sushengloong@gmail.com>
2019-12-12 14:50:57 -0800

    print unmanaged pipeline count by date

--- starport-core/src/main/scala/com/krux/starport/SummarizeUnmanagedPipelines.scala
+++ starport-core/src/main/scala/com/krux/starport/SummarizeUnmanagedPipelines.scala
@@ -9,8 +9,20 @@ object SummarizeUnmanagedPipelines extends StarportActivity {
     val pipelineStatuses = AwsDataPipeline.describePipeline(pipelinesInAws.toSeq: _*)
 
     logger.info("Unmanaged Pipeline Count By State:")
-    pipelineStatuses.groupBy(_._2.pipelineState).mapValues(_.size).toSeq.sortBy(_._2)(Ordering[Int].reverse)
+    pipelineStatuses.values.groupBy(_.pipelineState).mapValues(_.size).toSeq.sortBy(_._2)(Ordering[Int].reverse)
       .foreach { case (state, count) => logger.info(s"${state.getOrElse("Unknown")} -> $count") }
+
+    logger.info("Unmanaged Pipeline Count By Date:")
+    pipelineStatuses.values
+      .map { status =>
+        val date = status.creationTime.flatMap(_.split("T").headOption).getOrElse("Unknown")
+        (date, 1)
+      }
+      .groupBy(_._1)
+      .mapValues(_.map(_._2).sum)
+      .toSeq
+      .sortBy(_._1)(Ordering[String].reverse)
+      .foreach { case (date, count) => logger.info(s"$date -> $count") }
   }
 
   def main(args: Array[String]): Unit = {
